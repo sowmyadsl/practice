b9f867c454a8da599957b213efae6ec2d694cc15
James Higgins and Sowmya Dinavahi <james+sowmya@>
2017-03-21 09:52:43 -0700

    Add Dark and light buttons to the page to change the background and the text color of the page using jqery classes

--- js/scripts.js
+++ js/scripts.js
@@ -4,3 +4,15 @@ $(document).ready(function() {
     $(".initial-showing").hide();
   });
 });
+
+$(document).ready(function() {
+  $("button#dark").click(function() {
+    $("body").removeClass();
+    $("body").addClass("dark-background");
+  });
+
+  $("button#light").click(function() {
+    $("body").removeClass();
+    $("body").addClass("light-background");
+  });
+});
